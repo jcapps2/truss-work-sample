aab67a11c34e8ee8c7c73850db5d42d8e1e2a4c9
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2021-12-29 08:47:26 -0600

    added API call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const fetchData = async () => {
+  const res = await fetch("https://swapi.dev/api/planets/");
+  const data = res.json();
+  return data;
+};
+
 function App() {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    fetchData().then(res => {
+      console.log("RESOLVED: ", res);
+      setState(res);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>Truss Work Sample</h1>
+      {JSON.stringify(state.results)}
     </div>
   );
 }
