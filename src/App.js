4b7b6b11a02a1af2ace7e05ed170cd8199ec6ebd
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2021-12-29 10:43:49 -0600

    added pagination

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import Table from "./components/Table";
 import Loading from "./components/Loading";
 import "./App.css";
 
-const fetchData = async () => {
-  const res = await fetch("https://swapi.dev/api/planets/");
+const fetchData = async url => {
+  console.log(url);
+  const res = await fetch(url);
   const data = res.json();
   return data;
 };
 
 function App() {
   const [state, setState] = useState();
+  const [currentPage, setCurrentPage] = useState(
+    "https://swapi.dev/api/planets/"
+  );
 
   useEffect(() => {
-    fetchData().then(res => {
+    fetchData(currentPage).then(res => {
       console.log("RESOLVED: ", res);
       setState(res);
     });
-  }, []);
+  }, [currentPage]);
 
   const columns = useMemo(() => [
     {
@@ -90,6 +94,21 @@ function App() {
       <h1>Truss Work Sample</h1>
       <div className="table-container">
         {state ? <Table columns={columns} data={state.results} /> : <Loading />}
+        <div className="button-container">
+          <div>
+            {state && state.previous && (
+              <button onClick={() => setCurrentPage(state.previous)}>
+                Previous
+              </button>
+            )}
+          </div>
+
+          <div>
+            {state && state.next && (
+              <button onClick={() => setCurrentPage(state.next)}>Next</button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
